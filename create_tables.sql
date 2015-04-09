3e30bae2ba426de23751dfb60b0ec49bcfa5f6bb
Ryan Jensen <rjensen.professional@gmail.com>
2015-04-08 22:07:01 -0400

    Changed the mysql table schema

--- create_tables.sql
+++ create_tables.sql
@@ -5,14 +5,14 @@ CREATE TABLE `artist` (
   `first_name` varchar(30) DEFAULT '',
   `last_name` varchar(30) DEFAULT '',
   `instrument` varchar(60) DEFAULT '',
-  `genre` varchar(50) DEFAULT '',
+  `genre` varchar(60) DEFAULT '',
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 CREATE TABLE `album` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `artist_id` int(10) unsigned DEFAULT NULL,
-  `album` varchar(60) DEFAULT '',
+  `band_id` int(10) unsigned DEFAULT NULL,
+  `name` varchar(60) DEFAULT '',
   `release_date` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
@@ -24,12 +24,24 @@ CREATE TABLE `song` (
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
-CREATE TABLE `event` (
+CREATE TABLE `artist_for_band` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `band_id` int(10) unsigned NOT NULL,
   `artist_id` int(10) unsigned NOT NULL,
-  `name` varchar(40) DEFAULT '',
-  `time` datetime DEFAULT NULL,
-  `location_longitude` decimal(7,4) DEFAULT NULL,
-  `location_latitude` decimal(7,4) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+CREATE TABLE `band` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(60) DEFAULT NULL,
+  `genre` varchar(60) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+CREATE TABLE `contributer_for_song` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `song_id` int(10) unsigned NOT NULL,
+  `artist_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
