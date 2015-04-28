5b1d00b533218d894ba09d83310cdf379e129c6d
isugars <isugars@unca.edu>
2015-04-28 13:51:36 -0400

    work in progress

--- server.js
+++ server.js
@@ -443,7 +443,6 @@ var executeQuery = function(query, parameters, callback) {
     app.post('/api/photo',function(req,res){
       if(done==true){
         console.log(req.files);
-        res.end(message);
       }
     });
 
